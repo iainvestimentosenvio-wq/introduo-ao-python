e9cfd5890563b5a902a26b781f563fcf2c7a80f0
iainvestimentosenvio-wq <>
2025-10-29 19:16:59 +0000

    For Loops

---
+++ la_os_de_repeti_o/for_loops/main.py
@@ -0,0 +1,7 @@
+prices = [12.99, 8.50, 15.75, 23.00, 7.25]
+# Criando uma variavel 0 para ser somada :
+total = 0
+# Utilisando loop : 
+for price in prices:
+    total = total + price
+print("Total price for products:", total)
